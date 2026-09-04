516f6349b52d291493b5e156773656b9071474f4
kousei085875900 <kousei00168c@outlook.jp>
2026-09-05 07:10:37 +0900

    Create page.tsx

---
+++ app/projects/allport/page.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+export default function Home() {
+  return ( <main className="p-8"> 
+    <h1 className="text-2xl font-bold mb-4"> 
+      The goal of this project is to port various games to Scratch as faithfully as possible. 
+    </h1>
+    
+    <h1 className="text-2xl font-bold mb-4">
+      Things currently being transplanted
+    </h1> 
+    
+    <a href="https://キチガイ.com" target="_blank" rel="noopener noreferrer"className="text-blue-500 hover:underline"> 
+      fnf vs imposter scratch edition bata 
+    </a>
+    
+  </main>
+    );
+  }
