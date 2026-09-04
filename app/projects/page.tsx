2ec6cc527e21ab3e4d7fbff5cbbb597a9f572b61
kousei085875900 <kousei00168c@outlook.jp>
2026-09-04 21:53:05 +0900

    Update page title and add link to AllPort project

---
+++ app/projects/page.tsx
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+    <main className="p-8">
+
+
+     
+      <h1 className="text-2xl font-bold mb-4">
+        scratch projects
+      </h1>
+     
+      <Link href="/AllPort project" className="p-2 rounded hover:bg-red-500 transition text-sm">
+        allport
+      </Link>
+      
+      
+         
+      
+      　<a href="https://キチガイ.com" target="_blank" rel="noopener noreferrer"className="text-blue-500 hover:underline">
+  　　　　どっかに行くボタン
+　　　　</a>
+      
+    </main>
+  );
+}
