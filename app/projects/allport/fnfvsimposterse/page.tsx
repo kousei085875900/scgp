ffafff784853bb8d659c4ffed3051b8e73608b16
kousei085875900 <kousei00168c@outlook.jp>
2026-09-06 08:45:03 +0900

    Create page.tsx

---
+++ app/projects/allport/fnfvsimposterse/page.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main className="p-8">
+
+
+     
+      <h1 className="text-2xl font-bold mb-4">
+        fnf vs imposter scratch edition bata . 
+        
+        download page
+        
+      </h1> 
+      
+      
+      　<a href="https://github.com/kousei085875900/scgp/releases/download/fnfvsise/FNF.Vs.Imposter.v3.0.sb3" target="_blank" rel="noopener noreferrer"className="text-blue-500 hover:underline">
+  　　　　download
+　　　　</a>
+      
+    </main>
+  );
+}
