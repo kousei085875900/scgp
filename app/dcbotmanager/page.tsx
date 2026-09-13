873a9de1fc7728044a91dca8e2dc0a0a3109c8b4
kousei085875900 <kousei00168c@outlook.jp>
2026-09-13 18:20:44 +0900

    Update page.tsx

--- app/dcbotmanager/page.tsx
+++ app/dcbotmanager/page.tsx
@@ -192,13 +192,13 @@ export default function Home() {
         Discord token manager
       </h1>
 
-<h1 className="text-2xl font-bold mb-4">
+<p className="text-2xl font-bold mb-4">
         【注意】selfモードでユーザートークンでメッセージを送信する場合アカウントが停止される可能性があります
-      </h1>
+      </p>
 
       <h1 className="text-2xl font-bold text-red-500 mb-4">
         絶対にメインアカウントで試さないでください
-      </h1>
+      </p>
 
       <a href="https://mail.scgp.jp/" target="_blank" rel="noopener noreferrer"className="text-blue-500 hover:underline">
   　　　　簡易メールアドレスはこちらから作成できます
